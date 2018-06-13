cae94340f88faace9ad7ab7de26123aaaec2df73
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2018-06-13 13:44:53 +0200

    Task loading system.

--- washer/worker/__main__.py
+++ washer/worker/__main__.py
@@ -4,12 +4,20 @@ import os
 from .worker import prepare_app
 
 
+def load_tasks(filename):
+    from importlib.machinery import SourceFileLoader
+    return SourceFileLoader('washer.tasks', filename).load_module()
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--unset",
         nargs="*",
         help="Unregister environment var")
+    parser.add_argument(
+        "tasks_file",
+        help="Python file implementing washer tasks.")
 
     args = parser.parse_args()
 
@@ -18,6 +26,9 @@ def main():
     if args.unset:
         for name in args.unset:
             os.environ.pop(name)
+
+    # Load Washer tasks from user file.
+    load_tasks(args.tasks_file)
 
     # Prepare & run the app.
     app = prepare_app()
