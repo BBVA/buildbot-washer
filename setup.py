ebca7e54c1ee0b10c50a4aabf24b6b197e9b7611
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2018-06-14 14:33:57 +0200

    Added trove classifiers.

--- setup.py
+++ setup.py
@@ -2,11 +2,19 @@ import setuptools
 
 setuptools.setup(
     name="buildbot-washer",
-    version="0.0.2",
+    version="0.0.3",
     author="Roberto Abdelkader Martínez Pérez",
     author_email="robertomartinezp@gmail.com",
     description="Buildbot Utility Library",
     packages=setuptools.find_packages(exclude=["tests", "docs"]),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Framework :: Buildout :: Extension',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing'
+    ],
     install_requires=[
         "environconfig==1.7.0"
     ],
