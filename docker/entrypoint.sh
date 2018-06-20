bb09de3777ed98a9b4dc893e3fed52f43a7328a9
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2018-06-20 10:06:33 +0200

    Fix dockerfile.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 export BASEDIR="/washer" 
-/washer/ld-linux-x86-64.so.2 /washer/buildbot-worker "$@" --unset BASEDIR
+export PYTHONHOME="/washer"
+/washer/ld-linux-x86-64.so.2 /washer/buildbot-worker "$@" --unset PYTHONHOME
