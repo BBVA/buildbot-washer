10b148064674f68fe3729665bdb10c365ad5122f
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2018-06-20 08:52:49 +0200

    Version 0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="buildbot-washer",
-    version="0.0.6rc1",
+    version="0.0.6",
     author="Roberto Abdelkader Martínez Pérez",
     author_email="robertomartinezp@gmail.com",
     description="Buildbot Utility Library",
