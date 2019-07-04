5e3d5f81c9e8cda1980fb973fe67b894b68a3617
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-07-04 12:47:09 +0200

    Recursiverenderer is now defered.

--- tests/unit/master/test_20_recursiverender.py
+++ tests/unit/master/test_20_recursiverender.py
@@ -1,43 +1,53 @@
 from washer.master.recursiverender import recursiverender
 from buildbot.process.properties import Property, Secret
 
+from twisted.internet import defer
+import pytest_twisted as pt
 import pytest
 
 
+@pt.inlineCallbacks
 def test_nonrecursive_rendering():
     # recursiverender(<object>, <callback>)
     obj = Property('foo')
 
+    @defer.inlineCallbacks
     def callback(irenderable):
-        return irenderable
+        return (yield irenderable)
 
-    assert recursiverender(obj, callback) is obj
+    assert (yield recursiverender(obj, callback)) is obj
 
 
+@pt.inlineCallbacks
 def test_recursiverendering_calls_callback_if_renderable():
     obj = Property('foo')
 
     was_called = False
 
+    @defer.inlineCallbacks
     def callback(irenderable):
         nonlocal was_called
         was_called = True
+        yield None
 
-    recursiverender(obj, callback)
+    yield recursiverender(obj, callback)
 
     assert was_called
 
 
+@pt.inlineCallbacks
 def test_recursiverendering_dont_call_callback_if_not_renderable():
     obj = object()
 
     was_called = False
 
+    @defer.inlineCallbacks
     def callback(irenderable):
         nonlocal was_called
         was_called = True
+        yield None
 
-    recursiverender(obj, callback)
+    yield recursiverender(obj, callback)
 
     assert not was_called
 
@@ -46,19 +56,22 @@ def test_recursiverendering_dont_call_callback_if_not_renderable():
     (Property('foo'), "NotAProperty"),
     [Property('foo'), "NotAProperty"],
     {'foo': Property('foo'), 'bar': "NotAProperty"}])
+@pt.inlineCallbacks
 def test_simplestruct_nested_rendering(obj):
     was_called = False
 
+    @defer.inlineCallbacks
     def callback(irenderable):
         nonlocal was_called
         was_called = True
         assert irenderable == Property("foo")
-        return irenderable
+        return (yield irenderable)
 
-    assert recursiverender(obj, callback) == obj
+    assert (yield recursiverender(obj, callback)) == obj
     assert was_called
 
 
+@pt.inlineCallbacks
 def test_complexstruct_nested_rendering():
     obj = [{'foo1': (Property('foo'), Secret('bar')),
             'bar1': {'foo': 'foo', 'bar': Property('bar')}},
@@ -69,7 +82,8 @@ def test_complexstruct_nested_rendering():
                 {'foo2': (None, None),
                  'bar2': {'foo': 'foo', 'bar': None}}]
 
+    @defer.inlineCallbacks
     def callback(irenderable):
-        return None
+        return (yield None)
 
-    assert recursiverender(obj, callback) == expected
+    assert (yield recursiverender(obj, callback)) == expected
